a7443fbeaf06e2c7cd3a3a108729a66e7a40d293
Anshu Sinha <anshujuly2@gmail.com>
2023-04-25 21:38:17 +0530

    Add a code to serve static files using flask

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('my_site.html')
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
